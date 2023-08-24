94259fab622c242e08cbb426ef6e90cd0ac06c49
Masterobmij <143074214+Masterobmij@users.noreply.github.com>
2023-08-24 16:24:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php session_start() ?>
+<?php include("includes/header.php") ?>
+
+	<div class="wow fadeInDown">
+		<?php include("home/home_navbar_slider.php") ?>
+		<?php include("home/modal_signup.php") ?>
+		<?php include("home/modal_signin.php") ?>
+		<?php include("home/modal_seller.php") ?>
+		<?php include("home/home_content.php") ?>
+	</div>
+
+<?php include("includes/footer.php") ?>
